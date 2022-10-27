fc428622da9c0529928741d3298b13dfbd957802
thomas <tbe@pm.me>
2022-10-27 22:16:22 +0200

    Added code

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const port = new SerialPort({
   path: '/dev/ttyAMA0',
   baudRate: 9600,
 })
+
+
+port.write('main screen turn on', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+    console.log('message written')
+  })
+  
+  // Open errors will be emitted as an error event
+  port.on('error', function(err) {
+    console.log('Error: ', err.message)
+  })
